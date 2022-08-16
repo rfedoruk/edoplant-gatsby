6859fa57046565dc0ead239a1249946adbef7cc1
Roman Fedoruk <rfedoruk@students.kennesaw.edu>
2022-08-16 13:38:28 -0400

    added basic header with graphql query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import Layout from '../components/layout';
 
 export default function Blog ({data}) {
   console.log(data);
   return (
-  <div>
-    <h1>Welcome to My Blog</h1>
-    <h2>{data.wpPost.title}</h2>
-    <p>{data.wpPost.content}</p>
-  </div>
+    <Layout>
+      <h1>Welcome to My Blog</h1>
+      <h2>{data.wpPost.title}</h2>
+      <p>{data.wpPost.content}</p>
+    </Layout>
   )
 }
 
