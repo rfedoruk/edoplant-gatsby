3231b6a3480c8b8fbca3663513c308f6034475a4
Roman Fedoruk <rfedoruk@students.kennesaw.edu>
2022-08-16 12:52:27 -0400

    testing graphql query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-export default function Blog () {
-  return <h1>Welcome to My Blog</h1>
+export default function Blog ({data}) {
+  console.log(data);
+  return (
+  <div>
+    <h1>Welcome to My Blog</h1>
+    <h2>{data.wpPost.title}</h2>
+    <p>{data.wpPost.content}</p>
+  </div>
+  )
 }
+
+export const pageQuery = graphql`
+query MyQuery {
+  wpPost(databaseId: {eq: 1}) {
+    title
+    content
+  }
+}
+` 
